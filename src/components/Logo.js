99796d02fd38a2a8be7b05f129d69f8bd3f1a57e
Pierre RICARD <93609658+Pierre747@users.noreply.github.com>
2022-10-13 21:19:04 +0200

    Feat: Updated logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Logo() {
 	return (
 		<div className='logo'>
-			<img src='./logo192.png' alt='logo react' />
+			<img src='./earth-png.gif' alt='logo react' />
 			{/* Pour l'import des images React pense que l'on se situe dans le répertoire public */}
 			<h3>React World</h3>
 		</div>
